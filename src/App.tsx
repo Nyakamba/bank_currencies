9bb24191a0a554b11ea976f00372ce94038d38f8
Nyakamba <omwegaenock@gmail.com>
2024-11-15 21:30:14 +0300

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import CurrencyListItem from "./components/currency-list-item/CurrencyListItem";
 import SearchBar from "./components/search-bar/SearchBar";
+import { MdOutlineCurrencyExchange } from "react-icons/md";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <header className="flex items-center bg-green-700 text-white py-4 h-16 pl-4">
-        <h1 className="text-[32px] font-bold">Currency Exchange App</h1>
+      <header className="flex items-center bg-green-700 text-white py-4 h-16 pl-4 ">
+        <MdOutlineCurrencyExchange className="w-12 h-12" />
+
+        <div className="flex-1">
+          <h1 className="text-[32px] text-center  font-bold">
+            Currency Exchange App
+          </h1>
+        </div>
       </header>
       <div>
         <CurrencyListItem
